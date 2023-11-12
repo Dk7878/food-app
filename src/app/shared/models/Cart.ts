53916f017b89a84b98274042b317747fdf0d9916
Dk7878 <kosaldurgesh7878@gmail.com>
2023-11-12 12:52:55 -0500

    updated

--- src/app/shared/models/Cart.ts
+++ src/app/shared/models/Cart.ts
@@ -6,17 +6,16 @@ export class Cart {
   get totalPrice(): number {
     let totalPrice = 0;
     this.items.forEach(item => {
-      totalPrice += item.price;
+      totalPrice += item.price * item.quantity;
     });
-
     return totalPrice;
   }
-  
   // Add the totalPrice property to the class
   set totalPrice(value: number) {
     // You can implement logic to set the total price if needed
     // For example, you might want to throw an error or handle it differently
     // based on your application's requirements.
     console.log("Setting total price is not allowed directly.");
+    
   }
 }
